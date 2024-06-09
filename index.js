c3e7cddb43d18994344bfc62bae17422dc28df1d
sntwandy <sntwandy@gmail.com>
2024-06-09 02:19:09 -0400

    feat: ✨Add express.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Its working');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log('Listening on port: ' + port);
+})
