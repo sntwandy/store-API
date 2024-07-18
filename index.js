24956a2dfa3989705ff3534daa07b471336fd423
sntwandy <sntwandy@gmail.com>
2024-07-18 03:33:46 -0400

    feat: ✨Getting parameters

--- index.js
+++ index.js
@@ -12,9 +12,34 @@ app.get('/new-route', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res)  => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000
+    id,
+    name: 'Product 3',
+    price: 1400,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    productId,
+    categoryId,
+    name: 'Product 4',
+    price: 452,
   });
 });
 
